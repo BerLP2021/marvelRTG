f09d272a29a157b7c43309ae795db7d515b9e67c
Homer <Home@fdasdar>
2023-04-21 11:53:02 +0300

    Fixed markup on page404

--- src/components/pages/404.jsx
+++ src/components/pages/404.jsx
@@ -6,17 +6,13 @@ import error from '../../resources/404.png';
 const Page404 = () => {
 
   return (
-    <>
-       <div className='p404-wrapper'>
-            <div className="col">
-                <p className='p404-text'>Page doesn't exist</p>
-                <Link className='p404-link' to='/'>Back to <span>Main page</span></Link>
-            </div>
-            <img className="p404-decoration" src={error} alt="error404" />
-
-       </div>
-        
-    </>
+    <div className='p404-wrapper'>
+        <div className="col">
+            <p className='p404-text'>Page doesn't exist</p>
+            <Link className='p404-link' to='/'>Back to <span>Main page</span></Link>
+        </div>
+        <img src={error} alt="error404" />
+    </div>
   )
 }
 
